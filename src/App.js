cd53623406b4d9ff07f81533d3d5853405106d6d
AfyaaKT <afyaakhudur@gmail.com>
2023-08-26 16:30:30 +0300

    complete qoute generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import {  Route,  Routes } from 'react-router-dom'
-import Navbar from './component/Navbar';
-import Home from './component/Cartoon';
-import Ratings from './component/Ratings';
-import ResPage from './component/ResPage';
-import Quize from './component/Quize';
+
 import Quote from './component/Quote';
-import Todo from './component/Todo';
 
 
 
@@ -14,17 +8,7 @@ function App() {
  
   return (
     <div >
-      <Home/>
-      <Todo/>
       <Quote/>
-      <Quize/>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Ratings/>}/>
-        <Route path='/ResPage' element={<ResPage/>} />
-      </Routes>
-      
-
    </div>
   )
 
