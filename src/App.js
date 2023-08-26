bc6167f6c3a3a6c19502be34b7a2e750cc114ac3
AfyaaKT <afyaakhudur@gmail.com>
2023-08-26 16:41:30 +0300

    complete cartoon list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import {  Route,  Routes } from 'react-router-dom'
-import Navbar from './component/Navbar';
-import Home from './component/Cartoon';
-import Ratings from './component/Ratings';
-import ResPage from './component/ResPage';
-import Quize from './component/Quize';
-import Quote from './component/Quote';
-import Todo from './component/Todo';
 
+import Home from './component/Cartoon';
 
 
 
@@ -15,16 +8,7 @@ function App() {
   return (
     <div >
       <Home/>
-      <Todo/>
-      <Quote/>
-      <Quize/>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Ratings/>}/>
-        <Route path='/ResPage' element={<ResPage/>} />
-      </Routes>
-      
-
+    
    </div>
   )
 
