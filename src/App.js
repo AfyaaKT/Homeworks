a00265ed2a98821e6ecab3c4c0d386f8cacc041d
AfyaaKT <afyaakhudur@gmail.com>
2023-08-26 15:50:11 +0300

    fried chicken complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import {  Route,  Routes } from 'react-router-dom'
 import Navbar from './component/Navbar';
-import Home from './component/Cartoon';
 import Ratings from './component/Ratings';
 import ResPage from './component/ResPage';
-import Quize from './component/Quize';
-import Quote from './component/Quote';
-import Todo from './component/Todo';
 
 
 
@@ -14,10 +10,7 @@ function App() {
  
   return (
     <div >
-      <Home/>
-      <Todo/>
-      <Quote/>
-      <Quize/>
+      
       <Navbar/>
       <Routes>
         <Route path='/' element={<Ratings/>}/>
